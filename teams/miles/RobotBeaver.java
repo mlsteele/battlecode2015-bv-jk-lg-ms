2ad78baf38128b1884115d007e44d3d0a2541168
Miles Steele <miles@milessteele.com>
2015-01-05 23:03:39 -0500

    supply spawnlings

--- teams/miles/RobotBeaver.java
+++ teams/miles/RobotBeaver.java
@@ -6,11 +6,20 @@ import java.util.*;
 public class RobotBeaver extends Robot {
     RobotBeaver(RobotController rc) { super(rc); }
 
+    public static final int STARTING_SUPPLY = 1000;
+
     @Override
     public void run() {
         rc.setIndicatorString(0, "i am a RobotBeaver");
 
+        // Wait for supplies
+        while (rc.getSupplyLevel() <= STARTING_SUPPLY / 2) {
+            rc.yield();
+        }
+
         while(true) {
+            rc.setIndicatorString(2, "supply: " + rc.getSupplyLevel());
+
             if (rc.isCoreReady()) {
                 safeMove(Direction.NORTH);
             }
--- teams/miles/RobotHQ.java
+++ teams/miles/RobotHQ.java
@@ -15,6 +15,15 @@ public class RobotHQ extends Robot {
                 safeSpawn(Direction.NORTH, RobotType.BEAVER);
             }
 
+            // Supply spawnlings
+            try {
+                MapLocation supplyTargetLoc = rc.getLocation().add(Direction.NORTH);
+                RobotInfo   supplyTarget    = rc.senseRobotAtLocation(supplyTargetLoc);
+                if (supplyTarget != null && supplyTarget.supplyLevel < RobotBeaver.STARTING_SUPPLY / 2) {
+                    rc.transferSupplies(RobotBeaver.STARTING_SUPPLY, supplyTargetLoc);
+                }
+            } catch (GameActionException e) { }
+
             rc.yield();
         }
     }
