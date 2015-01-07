f4956220ccc524df35fad0066a7ab3a0733d228c
Miles Steele <miles@milessteele.com>
2015-01-06 19:37:41 -0500

    furbie mining

--- teams/seventeen/RobotBeaver.java
+++ teams/seventeen/RobotBeaver.java
@@ -6,9 +6,13 @@ import static battlecode.common.RobotType.*;
 import java.util.*;
 
 public class RobotBeaver extends Robot {
-    RobotBeaver(RobotController rc) { super(rc); }
-
     public static final int STARTING_SUPPLY = 1000;
+
+    private static final int BARRACKS_BUILD_DIST = 4;
+
+    private int distanceTraveled = 0;
+
+    RobotBeaver(RobotController rc) { super(rc); }
 
     @Override
     public void run() {
@@ -23,13 +27,36 @@ public class RobotBeaver extends Robot {
         while(true) {
             rc.setIndicatorString(2, "supply: " + rc.getSupplyLevel());
 
-            if (rc.isCoreReady()) buildBarracks();
-            if (rc.isCoreReady()) wander();
+            if (rc.isCoreReady() && distanceTraveled >= BARRACKS_BUILD_DIST) {
+                buildBarracks();
+            }
+
+            if (rc.isCoreReady()) mine();
+
+            if (rc.isCoreReady()) {
+                if (wander()) {
+                    distanceTraveled += 1;
+                }
+            }
 
             rc.yield();
         }
     }
 
+    // Attempt to mine.
+    // NOTE: this is beaver specific because of that constant.
+    private void mine() {
+        boolean shouldMine = rc.senseOre(rc.getLocation()) >= GameConstants.BEAVER_MINE_MAX;
+        if (shouldMine && rc.canMine()) {
+            try {
+                rc.mine();
+            } catch (GameActionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Attempt to build a barracks.
     private void buildBarracks() {
         Direction dir = randomDirection();
         if (rc.canBuild(dir, BARRACKS)) {
