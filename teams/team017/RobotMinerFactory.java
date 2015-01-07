a409393e368c4905275073934f5ecd01e58ae7cd
Miles Steele <miles@milessteele.com>
2015-01-07 17:58:51 -0500

    cleanup minerfactory

--- teams/team017/RobotMinerFactory.java
+++ teams/team017/RobotMinerFactory.java
@@ -16,27 +16,10 @@ public class RobotMinerFactory extends Robot {
         rc.setIndicatorString(0, "I am a RobotMinerFactory");
 
         while (true) {
-            // Spawn a miner
-            // Only if there is enough supply for them.
-            if (rc.isCoreReady()) {
-                Direction spawnDir = null;
-                int supplyAmount = RobotMiner.STARTING_SUPPLY;
-                if (rc.getSupplyLevel() > supplyAmount)
-                    spawnDir = spawn(MINER);
+            if (rc.isCoreReady() && rc.getSupplyLevel() >= RobotMiner.STARTING_SUPPLY)
+                spawn(MINER);
 
-                // Supply the spawnling.
-                if (spawnDir != null) {
-                    // Wait one round for the robot to spawn.
-                    rc.yield();
-                    MapLocation supplyTargetLoc = rc.getLocation().add(spawnDir);
-
-                    try {
-                        rc.transferSupplies(supplyAmount, supplyTargetLoc);
-                    } catch (GameActionException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
+            supplyNearbyEmpty(null, MINER, RobotMiner.STARTING_SUPPLY);
 
             rc.yield();
         }
